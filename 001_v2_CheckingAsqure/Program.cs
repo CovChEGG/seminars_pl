d14be0e322e854380073237961c367da74ab1dc7
CovChEGG <CovChEGG@gmail.com>
2021-12-19 22:53:29 +0700

    001 version 2 Checking a squre of numbers

---
+++ 001_v2_CheckingAsqure/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число =>");
+string valueString1 = Console.ReadLine();
+int valueInt1 = int.Parse(valueString1);
+Console.WriteLine("Введите второе число =>");
+string valueString2 = Console.ReadLine();
+int valueInt2 = int.Parse(valueString2);
+if (valueInt2 * valueInt2 == valueInt1)
+{
+    Console.Write("Первое число является квадратом второго числа");
+}
+else
+{
+        if (valueInt1*valueInt1==valueInt2)
+        {
+            Console.Write("Второе число является квадратом первого числа");
+        }
+        else
+        {
+            Console.Write("Числа не являются квадратами друг друга");
+        }
+}
