5efb7ce3861dc88fafd0bef3a5aacc0896ab38aa
CovChEGG <CovChEGG@gmail.com>
2021-12-19 23:20:29 +0700

    003 Number Converts to Name of the Day Of the Week

---
+++ 003_Number_2_DayOfWeek/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Input the number of the day of the week =>");
+string valueString = Console.ReadLine();
+int valueInt = int.Parse(valueString);
+if ((0 < valueInt) & (valueInt < 8))
+{
+    if (valueInt == 1)
+        Console.WriteLine("Monday");
+    if (valueInt == 2)
+        Console.WriteLine("Tuesday");
+    if (valueInt == 3)
+        Console.WriteLine("Wednesday");
+    if (valueInt == 4)
+        Console.WriteLine("Thursday");
+    if (valueInt == 5)
+        Console.WriteLine("Friday");
+    if (valueInt == 6)
+        Console.WriteLine("Saturday");
+    if (valueInt == 7)
+        Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("Incorrect input!!!");
+}
