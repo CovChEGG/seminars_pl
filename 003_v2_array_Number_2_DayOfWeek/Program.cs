bb17f4ce43a0054ac8031e715a49392afa525f63
CovChEGG <CovChEGG@gmail.com>
2021-12-19 23:31:47 +0700

    003 version 2 array using Number_2_DayOfWeek added

---
+++ 003_v2_array_Number_2_DayOfWeek/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input a number of the day of the week =>");
+string valueString = Console.ReadLine();
+int valueInt = int.Parse(valueString);
+string[] NameOfDays = { "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday", "Sunday" };
+if ((0 < valueInt) & (valueInt < 8))
+{
+    Console.WriteLine(NameOfDays[valueInt - 1]);
+}
+else
+{
+    Console.WriteLine("Wrong input!!!");
+}
