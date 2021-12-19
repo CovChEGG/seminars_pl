26b8618e642d0e8d70523454fcd1ac701359a127
CovChEGG <CovChEGG@gmail.com>
2021-12-20 00:27:42 +0700

    004 Max From 3 Numbers

---
+++ 004_MaxFrom3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] Array = new int[3];
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write("Input number ");
+    Console.Write(i + 1);
+    Console.Write(": ");
+    string valueString = Console.ReadLine();
+    int valueInt = int.Parse(valueString);
+    Array[i] = valueInt;
+}
+int max = Array[0];
+for (int i = 1; i < Array.Length; i++)
+{
+    if (Array[i] > max)
+    {
+        max = Array[i];
+    }
+}
+Console.Write("Maximum namber is: ");
+Console.WriteLine(max);
