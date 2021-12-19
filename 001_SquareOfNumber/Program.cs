9343a3ef67a6f7e4ff6f75446edd564b75a857a6
CovChEGG <CovChEGG@gmail.com>
2021-12-19 21:57:10 +0700

    001 Square of number added (double)

---
+++ 001_SquareOfNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Input Number =>");
+string valueString = Console.ReadLine();
+double valueDouble = double.Parse(valueString);
+Console.Write ("Square of Number ");
+Console.Write (valueDouble);
+Console.Write (" is ");
+Console.WriteLine(valueDouble * valueDouble);
