3d9c2587b203ba81848a4385a0d8604c2e69c96f
CovChEGG <CovChEGG@gmail.com>
2021-12-19 23:01:32 +0700

    002 2Numbers More or Less Check Added

---
+++ 002_2Numbers_MoreLess/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое число =>");
+string valueString1 = Console.ReadLine();
+int valueInt1 = int.Parse(valueString1);
+Console.WriteLine("Введите второе число =>");
+string valueString2 = Console.ReadLine();
+int valueInt2 = int.Parse(valueString2);
+if (valueInt1 > valueInt2)
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(valueInt1);
+    Console.Write("Меншее число ");
+    Console.WriteLine(valueInt2);
+}
+else
+{
+    if (valueInt1 < valueInt2)
+    {
+        Console.Write("Большее число ");
+        Console.WriteLine(valueInt2);
+        Console.Write("Меншее число ");
+        Console.WriteLine(valueInt1);
+
+    }
+    else
+    {
+        Console.Write("Числа равны");
+    }
+}
