dc85ee204afa4a0559f6d10117c44adeecdb877a
CovChEGG <CovChEGG@gmail.com>
2021-12-19 22:46:28 +0700

    001 Checking a square of number added

---
+++ 001_CheckingAsquare/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите квадрат числа =>");
+string valueString1 = Console.ReadLine();
+int valueInt1 = int.Parse(valueString1);
+Console.WriteLine("Введите само число =>");
+string valueString2 = Console.ReadLine();
+int valueInt2 = int.Parse(valueString2);
+if (valueInt2 * valueInt2 == valueInt1)
+{
+    Console.Write("Квадрат числа соответствует самому числу");
+}
+else
+{
+    Console.Write("Первое введеное число не является квадратом второго");
+}
