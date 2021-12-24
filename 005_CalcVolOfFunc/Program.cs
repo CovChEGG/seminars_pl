3419c31d7aa01d9c8f5e91d3f2fea8b026460a93
Pavel V. Glukhov <covchegg@pop-os.localdomain>
2021-12-25 01:06:56 +0700

    005_CalcVolOfFunc repeared

--- 005_CalcVolOfFunc/Program.cs
+++ 005_CalcVolOfFunc/Program.cs
@@ -1,14 +1,12 @@
-﻿// int Function(int x)
-// {
-//     int y = x * x + 3;
-//     return y;
-// };
+﻿int Function(int x)
+{
+    int y = x * x + 3;
+    return y;
+};
 
 Console.Clear();
 Console.WriteLine("Вычисления значения функции y = f(a)");
 Console.Write("Введите аргумент функции: ");
 string argumentString = Console.ReadLine();
 int argumentInt = int.Parse(argumentString);
-if()
-
-.WriteLine(Function(argumentInt));
+Console.WriteLine(Function(argumentInt));
