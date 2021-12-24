e827a532241e838b878d3f77e0c0f827abdc72fe
Pavel V. Glukhov <covchegg@pop-os.localdomain>
2021-12-25 00:58:28 +0700

    007 Show numbers form -N to N

---
+++ 007_fromNegativeN2positiveN/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input number N: ");
+string valueString = Console.ReadLine();
+Console.WriteLine();
+int valueInt = int.Parse(valueString);
+int i = -1 * valueInt;
+while (i <= valueInt )
+{
+    Console.Write(i);
+    Console.Write(" ");
+    i++;
+}
+Console.WriteLine();
