84175993c2ce537e1f32cb039101d494ee4d7542
Pavel V. Glukhov <covchegg@pop-os.localdomain>
2021-12-24 23:07:52 +0700

    006 parity check added

--- 005_CalcVolOfFunc/Program.cs
+++ 005_CalcVolOfFunc/Program.cs
@@ -1,12 +1,14 @@
-﻿int Function(int x)
-{
-    int y = x * x + 3;
-    return y;
-};
+﻿// int Function(int x)
+// {
+//     int y = x * x + 3;
+//     return y;
+// };
 
 Console.Clear();
 Console.WriteLine("Вычисления значения функции y = f(a)");
 Console.Write("Введите аргумент функции: ");
 string argumentString = Console.ReadLine();
 int argumentInt = int.Parse(argumentString);
-Console.WriteLine(Function(argumentInt));
+if()
+
+.WriteLine(Function(argumentInt));
---
+++ 006_parityСheck/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите проверяемое на чётность число: ");
+string valuetString = Console.ReadLine();
+int valueInt = int.Parse(valuetString);
+if ((valueInt % 2) != 0)
+{
+    Console.WriteLine("Введеное число является НЕЧЁТНЫМ");
+}
+else
+{
+    Console.WriteLine("Введеное число является ЧЁТНЫМ");
+}
