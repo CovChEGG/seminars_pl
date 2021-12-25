99f2f981ec2e223aaae118944b189ba1eb78761e
CovChEGG <CovChEGG@gmail.com>
2021-12-25 14:26:35 +0700

    adding Seminar2 test file with functions

---
+++ testing_Absolute/Program.cs
@@ -0,0 +1,22 @@
+﻿int value = ReadIntFromConsole ("Введите целое число: ");
+Console.WriteLine($"Модуль числа = {Absolute(value)}");
+
+int ReadIntFromConsole(string prompt)
+{
+    Console.WriteLine(prompt);
+    string answer = Console.ReadLine();
+    return Convert.ToInt32(answer);
+}
+
+
+int Absolute(int argument)
+{
+    if (argument >= 0)
+    {
+        return argument;
+    }
+    else
+    {
+        return -argument;
+    }
+}
