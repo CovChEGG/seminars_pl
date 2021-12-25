ff6c6e4642590703e33ba7d8d7ebc943dbb9cbd3
CovChEGG <CovChEGG@gmail.com>
2021-12-25 14:45:13 +0700

    added 009 LastDigitFrom3digitNumber modified to universal

---
+++ 009_LastDigitFrom3digitNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input 3-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of digit: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+// 
+for (int i = 1; i < digit; i++)
+{
+    number = number / 10;
+}
+number = number % 10;
+
+Console.WriteLine($"Number is: {number} ");
