81977a4e7a15222d00e04f37dff03a3e35cf201c
CovChEGG <CovChEGG@gmail.com>
2021-12-29 03:26:02 +0700

    021 v2 Universal realization for any int

---
+++ 021_v2_IsPolindrom_Universal/Program.cs
@@ -0,0 +1,40 @@
+﻿// 21. Программа проверяет введёное число на палиндромом.
+DateTime dt;
+Console.Clear();
+int ReadInt(string msg) // Выводит сообщение переданное на входе, осуществляет ввод корректного числа (с проверкой),
+{                       // на выходе возвращает значение в целых числах.
+    int valueInt;
+    while (true)
+    {
+        Console.Write(msg);                                      //Вывод полученного текста на экран
+        string? valueString = Console.ReadLine();
+        dt = DateTime.Now;
+        if (valueString != "-0")
+            if (int.TryParse(valueString, out valueInt)) break;      //Проверка на число и отправка значения в valueInt
+        Console.WriteLine("Incorrect input, try again...");
+    }
+    return valueInt;
+}
+
+bool IsPoly(int valueInt) // Функция проверки на палиндром - принимает число, возвращает true или false,.
+{
+    string valueString = valueInt.ToString();                   //преобразуем число в строку
+    int valueLength = valueString.Length;                       //вычисляем длину строки
+    for (int i = 0; i < (valueLength / 2); i++)
+    {
+        if (valueString[i] == valueString[valueLength - 1 - i]) continue;  //сравниваем элементы начала и конца
+        return false;                                           //возвращаем false при первом же не совпадении
+    }
+    return true;                                                //если все хорошо возвращаем true
+}
+
+int valueInt = ReadInt("Palindrome check. Enter the number: ");
+if (IsPoly(valueInt))
+{
+    Console.WriteLine("This is a palindrome");
+}
+else
+{
+    Console.WriteLine("Is'nt a palindrome");
+}
+Console.WriteLine(dt-DateTime.Now);
