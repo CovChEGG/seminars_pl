78670541c59de8afa7b1ebd9685cb13df81d77ac
CovChEGG <CovChEGG@gmail.com>
2021-12-31 00:00:39 +0700

    adding coments to 021_IsPolindrome

--- 021_IsPolindrom/Program.cs
+++ 021_IsPolindrom/Program.cs
@@ -1,26 +1,32 @@
 ﻿// 21. Программа проверяет пятизначное число на палиндромом.
 
 Console.Clear();
-int ReadInt(string msg)
-{
-    Console.Write(msg);
-    string? valueString = Console.ReadLine();
-    int valueInt = 0;
-    try
+int ReadInt(string msg) // Выводит сообщение переданное на входе, осуществляет ввод корректного числа (с проверкой),
+{                       // на выходе выдаёт значение в целых числах.
+    int valueInt;
+    while (true)
     {
-        valueInt = Convert.ToInt32(valueString);
-        if (!((valueInt > 9999) && (valueInt < 100000))) throw new Exception();
+        Console.Write(msg);
+        string? valueString = Console.ReadLine();
+        if (int.TryParse(valueString, out valueInt) && (valueInt > 9999) && (valueInt < 100000)) break;
+        else Console.WriteLine("Incorrect input, try again...");
     }
-    catch (System.Exception)
-    {
-        Console.WriteLine("Incorrect input");
-        Environment.Exit(1);
-    }
+    // try
+    // {
+    //     valueInt = Convert.ToInt32(valueString);
+    //     if ((valueInt < 9999) || (valueInt > 100000)) throw new Exception();
+    // }
+    // catch (System.Exception)
+    // {
+    //     Console.WriteLine("Incorrect input");
+    //     Environment.Exit(1);
+    // }
     return valueInt;
 }
-bool IsPoly(int valueInt1)
+
+bool IsPoly(int valueInt) // Функция проверки на палиндром.
 {
-    string valueString = valueInt1.ToString();
+    string valueString = valueInt.ToString();
     for (int i = 0; i < 2; i++)
     {
         if (valueString[i] == valueString[4 - i]) continue;
@@ -29,13 +35,13 @@ bool IsPoly(int valueInt1)
     return true;
 }
 
-int valueInt1 = ReadInt("Input number 1: ");
-if (IsPoly(valueInt1))
+int valueInt = ReadInt("Input 5 digit number: ");
+if (IsPoly(valueInt))
 {
-    Console.WriteLine("Is the Polindrome");
+    Console.WriteLine("This Is a palindrome");
 }
 else
 {
-    Console.WriteLine("Is'nt Polindrome");
+    Console.WriteLine("Is'nt a palindrome");
 }
-// Модифицировать то выявления полиндрома из любого числа.
+// Модифицировать до выявления полиндрома из любого числа.
