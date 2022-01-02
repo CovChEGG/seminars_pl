458142b14922883ca0e2ebcb37ba94f8885960ad
CovChEGG <covchegg@gmail.com>
2022-01-02 22:31:23 +0700

    starting write 023 Code

---
+++ 023_TableOfSquares/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+
+// Console.WriteLine("{0,10}   |{1,10}", x,y);
+// Console.SetCursorPosition(xa, ya);
+int n = 5;
+void PrintString(int StringNum, int ColumnNum)
+{
+    for (int i = 0; i < StringNum; i++)
+    {
+        int k = 5; //шаг по горизонтали, первая строка с отступом
+        for (int j = 1; j < ColumnNum; j++)
+        {
+
+            Console.SetCursorPosition(i, j*4);
+            Console.WriteLine($""
+            }
+        k = 0;
+    }
+};
