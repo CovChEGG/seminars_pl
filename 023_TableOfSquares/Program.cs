44060953fda4f35808fb9362af642ab03fcbe10b
CovChEGG <covchegg@gmail.com>
2022-01-04 15:08:11 +0700

    023 TableOfSquares - checking on Console Buffer Height added

--- 023_TableOfSquares/Program.cs
+++ 023_TableOfSquares/Program.cs
@@ -4,20 +4,49 @@
 // Console.SetCursorPosition(xa, ya);
 Console.Clear();
 
-int n = 99;
-int i = 0;
-int j = 0;
-int k = 8; //отступ между колонками
-for (; (i < 10) & (i <= n); i++) //вывод единиц по горизонтали
-{
-    Console.SetCursorPosition(i * k + k, 0);
-    Console.Write(i);
+int ReadIntCH(string msg) // Выводит сообщение переданное на входе, осуществляет
+{                         // ввод корректного числа (с проверкой на высоту буфера консоли),
+    int valueInt;         // на выходе выдаёт значение в целых числах.
+    while (true)
+    {
+        Console.Write(msg);
+        string? valueString = Console.ReadLine();
+        // if (int.TryParse(valueString, out valueInt) && (valueInt < 100000)) break;
+        if (int.TryParse(valueString, out valueInt) && (valueInt < 100000)) //проверка соответствия
+        {                                                                   //введёного числа
+            if (valueInt < (Console.BufferHeight * 10 - 10)) break;         //высоте консоли
+            else
+            {
+                Console.WriteLine("Incorrect input - small size of console, try less number...");
+            }
+        }
+
+
+        else Console.WriteLine("Incorrect input, try again...");
+    }
+    return valueInt;
 }
-i = 0;
+
+int n = ReadIntCS("Input number N: ");  //предельное число для вычилсения
+
+// Console.WriteLine($"Current buffer size: {Console.BufferWidth}x{Console.BufferHeight}");
+// Console.BufferHeight = 1000; //для Windows только!!! - для создания большей высоты в консоли
+
+Console.Clear();
+int i;
+int j;
+int k = 8; //отступ между колонками
+for (j = 0; (j < 10) & (j <= n); j++) //подписи столбцов с единицами по горизонтали
+{
+    Console.SetCursorPosition(j * k + k, 0);
+    Console.Write(j);
+}
+i = 0; //начальное значение десятков
+j = 1; //начальное значение отсчёта едениц - по условию оно равно 1 (но красивее и правильнее с 0)
 while ((i * 10 + j) <= n)
 {
-    Console.SetCursorPosition(j, i + 1);  // вывод десятков
-    Console.Write(i * 10);              // по вертикали
+    Console.SetCursorPosition(j, i + 1);  // вывод десятков 
+    Console.Write(i * 10);                // в первом столбце
     while (((i * 10 + j) <= n) && (j < 10))
     {
         Console.SetCursorPosition(j * k + k, i + 1);
@@ -29,5 +58,3 @@ while ((i * 10 + j) <= n)
 }
 Console.WriteLine();
 //s += ($"{i,5}^2 = {i*i}") - суммирует в строку
-
-
