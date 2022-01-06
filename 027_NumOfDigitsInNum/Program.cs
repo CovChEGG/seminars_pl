0863b0055e5983bcbbd23af1dc815ad47ff83a70
CovChEGG <covchegg@gmail.com>
2022-01-07 01:21:07 +0700

    027_NumOfDigitsInNum added

---
+++ 027_NumOfDigitsInNum/Program.cs
@@ -0,0 +1,19 @@
+﻿// 27. Определить количество цифр в числе
+int CountDigits(string msg) // Выводит сообщение переданное на входе, осуществляет
+{                           // проверку корректности числа,
+    string valueString=string.Empty;  // на выходе выдаёт количество элементов
+    double value;         
+    while (true)
+    {
+        Console.Write(msg + $": ");
+        valueString = Console.ReadLine() ?? "";
+        if (double.TryParse(valueString, out value)) break;
+        else Console.WriteLine("Incorrect input for int, try again...");
+    }
+
+    if(value == 0) return 1;
+    if(value < 0) return (valueString.Length-1);
+    return valueString.Length;
+}
+Console.Clear();
+Console.WriteLine($"Number of digits is: {CountDigits("Input Number")}");
