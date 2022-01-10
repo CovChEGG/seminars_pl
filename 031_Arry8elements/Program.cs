86922a32f5003e0a3ed455c977d4d88b9d788c0d
CovChEGG <covchegg@gmail.com>
2022-01-11 00:39:58 +0700

    031_Arry8elements added

---
+++ 031_Arry8elements/Program.cs
@@ -0,0 +1,22 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+void FillArrayRnd(int[] arrand, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arrand.Length; i++)
+    {
+        arrand[i] = rnd.Next(0, 999);
+    }
+}
+void PrintArray(int[] arprint)
+{
+    for (int i = 0; i < arprint.Length; i++)
+    {
+        Console.Write($"{arprint[i]} ");
+    }
+}
+
+int[] array = new int[8];
+FillArrayRnd(array,0,1234);
+PrintArray(array);
+Console.WriteLine();
