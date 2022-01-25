cca2f644bee79b2210dd6143fbfd32e53d188ab2
CovChEGG <covchegg@gmail.com>
2022-01-25 09:00:33 +0700

    010_Show2ndDigOfNum added

---
+++ 010_Show2ndDigOfNum/Program.cs
@@ -0,0 +1,26 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+Console.Clear();
+int ReadInt(string msg) // Выводит сообщение переданное на входе, 
+{                       // проверяет ввод числа на корректность (с проверкой),
+    int valueInt;       // на выходе выдаёт значение в целых числах.
+    while (true)
+    {
+        Console.Write(msg);
+        string? valueString = Console.ReadLine();
+        if (int.TryParse(valueString, out valueInt) 
+            && (valueInt > 99)
+            && (valueInt < 1000))
+            break;
+        else Console.WriteLine("Incorrect input, try again...");
+    }
+    return valueInt;
+}
+
+void ShowSecondDigital(int intNumber)
+{
+    string stringNumber = Convert.ToString(intNumber);
+    Console.WriteLine($"Second digit is: {stringNumber[1]}");
+}
+
+int number = ReadInt("Input 3 digit number: ");
+ShowSecondDigital(number);
