9e41e9441df415bac9817f139134476e0f3d6f8d
CovChEGG <covchegg@gmail.com>
2022-01-28 01:20:27 +0700

    038_SumNumberInOddIndex is done

--- 038_SumNumberInOddIndex/Program.cs
+++ 038_SumNumberInOddIndex/Program.cs
@@ -1,7 +1,38 @@
 ﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
-
-
-int Sum(int[] array)
+int SumOfNumberWithOddIndex(int[] array)
 {
-    int number +=array[i]
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2) sum += array[i];
+    return sum;
 }
+
+int[] FillArrayRandom(int[] arrayForFilling, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arrayForFilling.Length; i++)
+    {
+        arrayForFilling[i] = rnd.Next(min, max);
+    }
+    return arrayForFilling;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        int numOfElemForPrintInOneRaw = 12;
+        if (i % numOfElemForPrintInOneRaw == 0) Console.WriteLine();
+        Console.Write($"{arrayForPrint[i]}\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.Clear();
+
+int lengthOfArray = 10;
+
+int[] array = new int[lengthOfArray];
+array = FillArrayRandom(array, min: 0, max: 100);
+PrintArray(array);
+int sum = SumOfNumberWithOddIndex(array);
+Console.WriteLine($"Sum of elements in array with odd index is {sum}");
